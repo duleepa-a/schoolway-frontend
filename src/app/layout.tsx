f1df91fb2173b4f46e1453c3e08a8df987dff861
duleepa-a <duleepa24@gmail.com>
2025-06-19 22:02:16 +0530

    small fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 const poppins = Poppins({
   variable: "--font-poppins",
   subsets: ["latin"],
-  weight: ["400", "500", "600", "700"], // You can customize this
+  weight: ["400", "500", "600", "700"], 
 });
 
 export const metadata: Metadata = {
