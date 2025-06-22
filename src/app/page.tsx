48fd3372cd20f1c9a71df21a4fbecf291a4b89a1
duleepa-a <duleepa24@gmail.com>
2025-06-22 15:33:19 +0530

    home page updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
 import Navbar from  "./components/Navbar";
 import HeroSection from "./components/HeroSection";
 import WhatWeOffer from "./components/WhatWeOffer";
+import AppDetails from "./components/AppDetails";
+import HomeInsightBanner from "./components/HomeInsightBanner";
+import HomeInfoCards from "./components/HomeInfoCards";
+import HomeTestimonials from "./components/HomeTestimonials";
+import Footer from "./components/Footer";
+import AppLink from "./components/AppLink";
 
 
 export default function Home() {
@@ -9,6 +15,12 @@ export default function Home() {
       <Navbar />
       <HeroSection />
       <WhatWeOffer/>
+      <AppDetails/>
+      <HomeInsightBanner/>
+      <HomeInfoCards/>
+      <HomeTestimonials/>
+      <AppLink/>
+      <Footer/>
     </main>
   );
 }
