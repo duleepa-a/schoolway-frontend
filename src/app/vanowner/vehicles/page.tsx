0223f0f31cb2b56e9650cb0a61aca0a4d37cf42e
duleepa-a <duleepa24@gmail.com>
2025-07-03 13:15:19 +0530

    common heading component added to pages

--- src/app/vanowner/vehicles/page.tsx
+++ src/app/vanowner/vehicles/page.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import {FaBell, FaMoon } from 'react-icons/fa';
 import Image from 'next/image';
 import VehicleCard from './VehicleCard';
+import TopBar from '@/app/dashboardComponents/TopBar';
+import { FaSearch,FaChevronDown } from 'react-icons/fa';
 
 
 const vehiclesPage = () => {
@@ -42,21 +44,19 @@ const vehiclesPage = () => {
   return (
     <section className="p-6 md:p-10 min-h-screen w-full">
         {/* Top Right Icons */}
-        <div className="flex justify-between items-center mt-10 mb-2.5">
-          <div>
-            <h2 className="text-2xl font-bold text-gray-800 mb-6">My Vehicles</h2>
-          </div>
-          <div className='bg-white flex justify-end items-center gap-4 mb-6 py-1 px-6 rounded-4xl w-fit'>
-            <FaBell className="text-gray-500 hover:text-gray-700 cursor-pointer ml-5" />
-            <FaMoon className="text-gray-500 hover:text-gray-700 cursor-pointer" />
-            <Image
-              src="/Images/male_pro_pic_placeholder.png"
-              alt="Profile"
-              width={50}
-              height={50}
-              className="rounded-full object-cover border ml-14"
-            />
-          </div>
+        <TopBar heading = "My Vehicles" />
+
+        <div className="flex flex-col md:flex-row md:items-center md:justify-between lg:justify-start  mb-4 gap-4">
+                <div className="relative w-full md:w-1/3">
+                  <FaSearch className="absolute left-3 top-3 text-gray-400" />
+                  <input
+                    type="text"
+                    placeholder="Search vehicle"
+                    className="
+                    w-full pl-10 pr-4 py-2 border border-gray-300 rounded-md  bg-search-bar-bg"
+                  />
+                </div>
+                
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
