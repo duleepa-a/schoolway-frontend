d719ba3ec250a6ec04ad970075eb963611fde605
Def119 <lehanselaka@gmail.com>
2025-07-18 05:26:15 +0530

    parameter fixes : van fetch

--- src/app/api/van_service/drivers/[id]/route.ts
+++ src/app/api/van_service/drivers/[id]/route.ts
@@ -6,7 +6,7 @@ export async function GET(
   { params }: { params: { id: string } }
 ) {
   try {
-    const { id } = params;
+    const { id } = await  params;
 
     // Get driver profile with user data
     const driver = await prisma.driverProfile.findFirst({
@@ -26,7 +26,7 @@ export async function GET(
             email: true,
             district: true,
             mobile: true,
-            city: true,
+            // city: true,
             nic: true,
             dp: true,
             activeStatus: true,
