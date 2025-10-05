251d6f75716ca544e5ae7734e22e526a3ea7f4b6
duleepa-a <duleepa24@gmail.com>
2025-10-05 23:45:04 +0530

    fix

--- src/app/api/admin/schools/SchoolGuardians/[schoolId]/route.tsx
+++ src/app/api/admin/schools/SchoolGuardians/[schoolId]/route.tsx
@@ -1,25 +1,25 @@
-// import { NextResponse } from 'next/server';
-// import prisma from '@/lib/prisma';
+import { NextResponse } from 'next/server';
+import prisma from '@/lib/prisma';
 
-// export async function GET(req: Request, { params }: { params: { schoolId: string } }) {
-//   try {
-//     const schoolId = params.schoolId;
-//     if (!schoolId) {
-//       return NextResponse.json({ error: 'Missing schoolId parameter' }, { status: 400 });
-//     }
+export async function GET(req: Request, { params }: { params: { schoolId: string } }) {
+  try {
+    const schoolId = params.schoolId;
+    if (!schoolId) {
+      return NextResponse.json({ error: 'Missing schoolId parameter' }, { status: 400 });
+    }
 
-//     const guardians = await prisma.schoolGuardian.findMany({
-//       where: { schoolId: Number(schoolId) },
-//       select: {
-//         guardianId: true,
-//         firstName: true,
-//         lastName: true,
-//         email: true,
-//         },
-//     });
+    const guardians = await prisma.schoolGuardian.findMany({
+      where: { schoolId: Number(schoolId) },
+      select: {
+        guardianId: true,
+        firstName: true,
+        lastName: true,
+        email: true,
+        },
+    });
 
-//     return NextResponse.json({ guardians });
-//   } catch (error) {
-//     return NextResponse.json({ error: 'Failed to fetch guardians', details: String(error) }, { status: 500 });
-//   }
-// }
+    return NextResponse.json({ guardians });
+  } catch (error) {
+    return NextResponse.json({ error: 'Failed to fetch guardians', details: String(error) }, { status: 500 });
+  }
+}
